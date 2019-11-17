b460c3278e4ff2b1886c3ef4322546a2d8bc8b85
Aliaksei Petrusevich <water.waters@yandex.by>
2019-11-17 19:46:26 +0300

    Add base model for search view controller

---
+++ Travel-App/Scenes/Search/View/SearchViewProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  SearchViewProtocol.swift
+//  Travel-App
+//
+//  Created by Антон Иванов on 11/17/19.
+//  Copyright © 2019 companyName. All rights reserved.
+//
+
+protocol SearchViewProtocol: class{
+    
+}
