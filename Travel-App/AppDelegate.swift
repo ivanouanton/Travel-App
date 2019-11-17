f5ea184ad8cdc61a11d73e2b1da098d5bc744974
Aliaksei Petrusevich <water.waters@yandex.by>
2019-11-17 19:35:06 +0300

    Cofigurate project without storyboard

--- Travel-App/AppDelegate.swift
+++ Travel-App/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let navigationController = UINavigationController()
+        
+        navigationController.setViewControllers([ViewController()], animated: false)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
