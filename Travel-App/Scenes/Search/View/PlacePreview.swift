452c70b9e81795667915ef27b3703d8cd1b1d9f9
Aliaksei Petrusevich <water.waters@yandex.by>
2019-11-19 02:02:07 +0300

    Add text labels for place subview

--- Travel-App/Scenes/Search/View/PlacePreview.swift
+++ Travel-App/Scenes/Search/View/PlacePreview.swift
@@ -14,9 +14,57 @@ class PlacePreview: UIView {
         let image = UIImageView()
         image.image = UIImage(named: "place")
         image.translatesAutoresizingMaskIntoConstraints = false
-        
-        image.contentMode = .scaleAspectFit
+        image.backgroundColor = .red
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
         return image
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Marlow & Sons"
+        label.textAlignment = .left
+        label.font = UIFont(name: "Avenir", size: 16)!
+   
+        label.textColor = UIColor(named: "heavy")
+        return label
+    }()
+    
+    private lazy var directionsButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Directions", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 14)!
+        button.setTitleColor(UIColor(named: "pantone"), for: .normal)
+        button.layer.cornerRadius = 5
+        button.layer.backgroundColor = UIColor(named: "pantone")?.withAlphaComponent(0.2).cgColor
+        return button
+    }()
+    
+    private lazy var infoButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Info", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 14)!
+        button.setTitleColor(UIColor(named: "pantone"), for: .normal)
+        button.layer.cornerRadius = 5
+        button.layer.backgroundColor = UIColor(named: "pantone")?.withAlphaComponent(0.2).cgColor
+        return button
+    }()
+    
+    private lazy var userLocationLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = """
+        81 Broadway
+        Brooklyn, NY 11249
+        """
+        label.numberOfLines = 2
+        label.textAlignment = .left
+        label.font = UIFont(name: "Avenir", size: 14)!
+        label.textColor = UIColor(named: "silver")
+        return label
     }()
     
     override init(frame: CGRect) {
@@ -24,6 +72,8 @@ class PlacePreview: UIView {
         self.setupUI()
         self.setupConstraints()
     }
+    
+    
     
     
     required init?(coder aDecoder: NSCoder) {
@@ -34,6 +84,10 @@ class PlacePreview: UIView {
         self.layer.cornerRadius = 7
         
         self.addSubview(self.placeImage)
+        self.addSubview(self.titleLabel)
+        self.addSubview(self.directionsButton)
+        self.addSubview(self.infoButton)
+        self.addSubview(self.userLocationLabel)
     }
     
     private func setupConstraints(){
@@ -43,6 +97,23 @@ class PlacePreview: UIView {
             self.placeImage.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             self.placeImage.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.35),
             
+            self.titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+            self.titleLabel.leftAnchor.constraint(equalTo: self.placeImage.rightAnchor, constant: 8),
+            self.titleLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
+            
+            self.directionsButton.bottomAnchor.constraint(equalTo: self.placeImage.bottomAnchor),
+            self.directionsButton.leftAnchor.constraint(equalTo: self.placeImage.rightAnchor, constant: 8),
+            self.directionsButton.heightAnchor.constraint(equalToConstant: 35),
+            self.directionsButton.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.33),
+            
+            self.infoButton.bottomAnchor.constraint(equalTo: self.placeImage.bottomAnchor),
+            self.infoButton.leftAnchor.constraint(equalTo: self.directionsButton.rightAnchor, constant: 12),
+            self.infoButton.heightAnchor.constraint(equalToConstant: 35),
+            self.infoButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
+            
+            self.userLocationLabel.bottomAnchor.constraint(equalTo: self.infoButton.topAnchor, constant: -8),
+            self.userLocationLabel.leftAnchor.constraint(equalTo: self.placeImage.rightAnchor, constant: 8),
+            self.userLocationLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
             
             ])
     }
