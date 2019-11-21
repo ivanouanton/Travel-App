22abdbe4ae1d5e48a8051fea207b6f152fed2ce5
Aliaksei Petrusevich <water.waters@yandex.by>
2019-11-22 01:34:18 +0300

    Add settings table

--- Travel-App/Scenes/Settings/View/SettingsViewController.swift
+++ Travel-App/Scenes/Settings/View/SettingsViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 final class SettingsViewController: UIViewController{
     var presenter: SettingsPresenterProtocol!
+    var options = [(title: String, options: [String])]()
+    
+    private lazy var settingTable: UITableView = {
+        let table = UITableView.init(frame: .zero, style: UITableView.Style.grouped)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(SettingTableViewCell.self, forCellReuseIdentifier: "SettingTableViewCell")
+        table.dataSource = self
+        return table
+    }()
     
     override func loadView() {
         super.loadView()
@@ -17,18 +26,55 @@ final class SettingsViewController: UIViewController{
         self.setupUI()
         self.setupConstraints()
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.presenter.getSettingsProperty()
+    }
 }
 
 extension SettingsViewController{
     func setupUI(){
         self.view.backgroundColor = .white
+        self.view.addSubview(self.settingTable)
     }
     
     func setupConstraints(){
-        
+        NSLayoutConstraint.activate([
+            self.settingTable.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.settingTable.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            self.settingTable.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            self.settingTable.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 }
 
 extension SettingsViewController: SettingsViewProtocol{
+    func updateTable(with options: [(title: String, options: [String])]) {
+        self.options = options
+        self.settingTable.reloadData()
+    }
+}
+
+extension SettingsViewController: UITableViewDataSource{
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        print(options.count)
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(self.options[section].options.count)
+        return self.options[section].options.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.options[section].title
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.settingTable.dequeueReusableCell(withIdentifier: "SettingTableViewCell", for: indexPath) as! SettingTableViewCell
+        cell.textLabel?.text = self.options[indexPath.section].options[indexPath.row]
+        return cell
+    }
 }
--- Travel-App/Scenes/Settings/View/SettingsViewProtocol.swift
+++ Travel-App/Scenes/Settings/View/SettingsViewProtocol.swift
@@ -7,5 +7,5 @@
 //
 
 protocol SettingsViewProtocol: class{
-    
+    func updateTable(with options: [(title: String, options: [String])])
 }
