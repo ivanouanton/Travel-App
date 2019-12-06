2866fa3e3b6eb70e9c78d6f47a3e0c99466ddf35
Aliaksei Petrusevich <water.waters@yandex.by>
2019-12-06 07:20:54 +0300

    Set duration configuration UI

--- Travel-App/Scenes/Search/TourPreferensesView/PreferenceBoardViewController.swift
+++ Travel-App/Scenes/Search/TourPreferensesView/PreferenceBoardViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class PreferenceBoardViewController: UIViewController {
 
     @IBOutlet weak var interestsCollection: UICollectionView!
+    @IBOutlet weak var durationCollection: UICollectionView!
     var names = ["Anders", "Kristian", "Sofia", "John", "Jenny", "Lina"]
     var colors = [UIColor(named: "heavy")!,
                   UIColor(named: "onyx")!,
@@ -18,6 +19,7 @@ class PreferenceBoardViewController: UIViewController {
                   UIColor(named: "silver")!,
                   UIColor(named: "smokyTopaz")!,
                   UIColor(named: "heavy")!]
+    var durations = ["A Few Hours", "Half Day", "Full Day"]
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,30 +30,62 @@ class PreferenceBoardViewController: UIViewController {
     func registerNib() {
         let nib = UINib(nibName: InterestsCollectionViewCell.nibName, bundle: nil)
         interestsCollection?.register(nib, forCellWithReuseIdentifier: InterestsCollectionViewCell.reuseIdentifier)
+        
+        let nibDuration = UINib(nibName: SettingOptionCollectionViewCell.nibName, bundle: nil)
+        durationCollection?.register(nibDuration, forCellWithReuseIdentifier: SettingOptionCollectionViewCell.reuseIdentifier)
     }
 }
 
 extension PreferenceBoardViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return names.count
+        
+        switch collectionView {
+        case self.interestsCollection:
+            return names.count
+        case self.durationCollection:
+            return durations.count
+        default:
+            return 0
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
       
-        let cell = self.interestsCollection.dequeueReusableCell(withReuseIdentifier: InterestsCollectionViewCell.reuseIdentifier,
-                                                      for: indexPath) as! InterestsCollectionViewCell
-        let name = names[indexPath.row]
-        cell.configureCell(name: name, image: UIImage(named: "create-tour")!, color: colors[indexPath.row])
-        return cell
-
+        switch collectionView {
+        case self.interestsCollection:
+            let cell = self.interestsCollection.dequeueReusableCell(withReuseIdentifier: InterestsCollectionViewCell.reuseIdentifier,
+                                                          for: indexPath) as! InterestsCollectionViewCell
+            let name = names[indexPath.row]
+            cell.configureCell(name: name, image: UIImage(named: "create-tour")!, color: colors[indexPath.row])
+            return cell
+        case self.durationCollection:
+            let cell = self.durationCollection.dequeueReusableCell(withReuseIdentifier: SettingOptionCollectionViewCell.reuseIdentifier,
+                                                          for: indexPath) as! SettingOptionCollectionViewCell
+            let name = durations[indexPath.row]
+            cell.configureCell(text: name, actImage: nil, defImage: nil)
+            return cell
+        default:
+            return UICollectionViewCell()
+        }
     }
 }
 
 extension PreferenceBoardViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let cell = collectionView.cellForItem(at: indexPath) as? InterestsCollectionViewCell{
-            
-        }
+//        if let cell = collectionView.cellForItem(at: indexPath) as? InterestsCollectionViewCell{
+//
+//        }
 
     }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        if collectionView == durationCollection{
+//            let width = self.durationCollection.frame.height
+//            let cellWidth = (width - 92)/3
+//            return CGSize(width: cellWidth, height: self.durationCollection.frame.height)
+//
+//        }
+//
+//        return CGSize(width: 10, height: 10)
+//    }
 }
