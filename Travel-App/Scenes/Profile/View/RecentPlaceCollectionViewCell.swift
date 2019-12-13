a4a061c13b7cc8f92e105ef619a4f948a5cab3d4
Aliaksei Petrusevich <water.waters@yandex.by>
2019-12-13 03:55:22 +0300

    Add recent places xib

---
+++ Travel-App/Scenes/Profile/View/RecentPlaceCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecentPlaceCollectionViewCell.swift
+//  Travel-App
+//
+//  Created by Антон Иванов on 12/13/19.
+//  Copyright © 2019 companyName. All rights reserved.
+//
+
+import UIKit
+
+class RecentPlaceCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
