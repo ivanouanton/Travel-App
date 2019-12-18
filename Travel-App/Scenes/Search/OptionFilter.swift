2dad61f34052fcf11aeb1c1b77daf97815c0da4a
Aliaksei Petrusevich <water.waters@yandex.by>
2019-12-18 09:45:20 +0300

    Update filter view

--- Travel-App/Scenes/Search/OptionFilter.swift
+++ Travel-App/Scenes/Search/OptionFilter.swift
@@ -13,7 +13,6 @@ class OptionFilter: UIView {
     weak var delegate: OptionFilterDelegate?
     
     private var price = ["Free", "€", "€€"]
-    private var visitOptions = ["Must visit", "Visited"]
     
     var filterButtonHeight: CGFloat = 48
         
@@ -62,7 +61,20 @@ class OptionFilter: UIView {
         return button
     }()
     
-    private lazy var visitFilterButton: UIButton = {
+    private lazy var visitedFilterButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Visited", for: .normal)
+        button.titleLabel?.font = UIFont(name: "AvenirNextLTPro-Demi", size: 14)
+        button.setTitleColor(UIColor(named: "pantone"), for: .normal)
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor(named: "silver")?.cgColor
+        button.setBackgroundColor(color: UIColor(named: "white")!, forState: .normal)
+        button.addTarget(self, action: #selector(selectedVisetedFilter), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var mustVisitFilterButton: UIButton = {
         let button = UIButton()
         button.setTitle("Must visit", for: .normal)
         button.titleLabel?.font = UIFont(name: "AvenirNextLTPro-Demi", size: 14)
@@ -71,7 +83,7 @@ class OptionFilter: UIView {
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor(named: "silver")?.cgColor
         button.setBackgroundColor(color: UIColor(named: "white")!, forState: .normal)
-        button.addTarget(self, action: #selector(selectedVisetFilter), for: .touchUpInside)
+        button.addTarget(self, action: #selector(selectedMustVisitFilter), for: .touchUpInside)
         return button
     }()
     
@@ -81,9 +93,10 @@ class OptionFilter: UIView {
         stack.axis = .horizontal
         stack.distribution = .fillEqually
         stack.alignment = .fill
-        stack.spacing = 48
+        stack.spacing = 20
         stack.addArrangedSubview(self.priceFilterButton)
-        stack.addArrangedSubview(self.visitFilterButton)
+        stack.addArrangedSubview(self.visitedFilterButton)
+        stack.addArrangedSubview(self.mustVisitFilterButton)
         return stack
     }()
         
@@ -106,36 +119,22 @@ class OptionFilter: UIView {
         return table
     }()
     
-    private lazy var visitTable: UITableView = {
-        let table = UITableView.init(frame: .zero, style: UITableView.Style.grouped)
-        table.translatesAutoresizingMaskIntoConstraints = false
-        let settingsCell = UINib(nibName: OptionFilterTableViewCell.nibName, bundle: nil)
-        table.register(settingsCell, forCellReuseIdentifier: OptionFilterTableViewCell.reuseIdentifier)
-        table.backgroundColor = UIColor(named: "white")
-        table.separatorStyle = .none
-        table.layer.cornerRadius = 5
-        table.layer.shadowColor = UIColor.black.cgColor
-        table.layer.shadowOpacity = 0.3
-        table.layer.shadowOffset = .zero
-        table.layer.shadowRadius = 3
-        table.layer.masksToBounds = false
-        table.isScrollEnabled = false
-        table.dataSource = self
-        table.delegate = self
-        return table
-    }()
-    
-    
     // MARK: - Methods
     
     @objc func selectedPriceFilter(){
         self.delegate?.didPressedFilterButton(with: 247)
+        self.filterButton.setTitle("Filter by: Price", for: .normal)
         self.addfiterTable()
     }
     
-    @objc func selectedVisetFilter(){
+    @objc func selectedVisetedFilter(){
         self.delegate?.didPressedFilterButton(with: 190)
-        self.addfiterVisitTable()
+        self.filterButton.setTitle("Filter by: Visited", for: .normal)
+    }
+    
+    @objc func selectedMustVisitFilter(){
+        self.delegate?.didPressedFilterButton(with: 190)
+        self.filterButton.setTitle("Filter by: Must visit", for: .normal)
     }
     
     @objc func showFilter(){
@@ -176,17 +175,6 @@ class OptionFilter: UIView {
         ])
     }
     
-    func addfiterVisitTable(){
-        self.addSubview(self.visitTable)
-        
-        NSLayoutConstraint.activate([
-            self.visitTable.leftAnchor.constraint(equalTo: visitFilterButton.leftAnchor),
-            self.visitTable.topAnchor.constraint(equalTo: self.visitFilterButton.topAnchor),
-            self.visitTable.widthAnchor.constraint(equalTo: self.visitFilterButton.widthAnchor),
-            self.visitTable.heightAnchor.constraint(equalToConstant: 107),
-        ])
-    }
-    
     // MARK: - Life Cicle
 
     override init(frame: CGRect) {
@@ -223,55 +211,33 @@ extension OptionFilter: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        if tableView == self.filterTable{
-            return self.price.count
-        } else if tableView == self.visitTable{
-            return self.visitOptions.count
-        } else{
-            return 0
-        }
+        return self.price.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: OptionFilterTableViewCell.reuseIdentifier, for: indexPath) as! OptionFilterTableViewCell
-        
-        if tableView == self.filterTable{
-            cell.setupLabel(with: self.price[indexPath.row])
-        }else{
-            cell.setupLabel(with: self.visitOptions[indexPath.row])
-            if indexPath.row == 0{
-                cell.setupAsFirstRow()
-            }
-        }
-        return cell
+        cell.setupLabel(with: self.price[indexPath.row])
 
+        return cell
     }
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
 
-        if tableView == self.filterTable{
-           let view = UIView()
-           let label = Label()
-           label.text = "Price"
-           label.font = UIFont(name: "AvenirNextLTPro-Regular", size: 16)
-           label.textColor = UIColor(named: "silver")
-           label.translatesAutoresizingMaskIntoConstraints = false
-           
-           view.addSubview(label)
-           
-           NSLayoutConstraint.activate([
-               label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 15),
-               label.topAnchor.constraint(equalTo: view.topAnchor, constant: 14),
-               label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
-           ])
-            return view
-            
-           }else{
-               let view = UIView()
-               view.frame.size.height = 0
-               return view
-           }
+        let view = UIView()
+        let label = Label()
+        label.text = "Price"
+        label.font = UIFont(name: "AvenirNextLTPro-Regular", size: 16)
+        label.textColor = UIColor(named: "silver")
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(label)
+
+        NSLayoutConstraint.activate([
+           label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 15),
+           label.topAnchor.constraint(equalTo: view.topAnchor, constant: 14),
+           label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
+        ])
+        return view
     }
     
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
@@ -285,13 +251,8 @@ extension OptionFilter: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView == self.filterTable{
-            self.filterTable.removeFromSuperview()
-            self.priceFilterButton.titleLabel?.text = self.price[indexPath.row]
-        }else if tableView == self.visitTable{
-            self.visitTable.removeFromSuperview()
-            self.visitFilterButton.titleLabel?.text = self.visitOptions[indexPath.row]
-        }
+        self.filterTable.removeFromSuperview()
+        self.priceFilterButton.titleLabel?.text = self.price[indexPath.row]
         self.delegate?.didPressedFilterButton(with: self.filterView.frame.height+self.filterButton.frame.height)
     }
 }
