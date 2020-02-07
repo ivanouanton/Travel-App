8944b8717e819d871bd0ea0eee4b158c5ce35748
Anton Ivanou <ivanouanton@gmail.com>
2020-02-07 10:40:18 +0300

    Add custom view for audio player

---
+++ Travel-App/Scenes/Search/PlaceInfo/AudioPlayerView.swift
@@ -0,0 +1,31 @@
+//
+//  AudioPlayerView.swift
+//  Travel-App
+//
+//  Created by Anton Ivanov on 2/7/20.
+//  Copyright © 2020 companyName. All rights reserved.
+//
+
+import UIKit
+
+class AudioPlayerView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit(){
+        Bundle.main.loadNibNamed("AudioPlayerView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
