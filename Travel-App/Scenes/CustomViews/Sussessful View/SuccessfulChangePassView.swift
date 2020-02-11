26b2d480256c4627105efad32282cc942fb2375f
Anton Ivanou <ivanouanton@gmail.com>
2020-02-11 16:32:17 +0300

    Add successfulView

---
+++ Travel-App/Scenes/CustomViews/Sussessful View/SuccessfulChangePassView.swift
@@ -0,0 +1,32 @@
+//
+//  SuccessfulChangePassView.swift
+//  Travel-App
+//
+//  Created by Anton Ivanov on 2/11/20.
+//  Copyright © 2020 companyName. All rights reserved.
+//
+
+import UIKit
+
+class SuccessfulChangePassView: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
